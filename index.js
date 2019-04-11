d6f0da4c6dfe841f29a48de5a0137f98381889d4
ah100101 <ah100101@gmail.com>
2019-04-11 00:01:54 -0500

    created stop watch with moment using elapsed time from start

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 import './styles.scss'
+import moment from 'moment'
 
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Welcome to the Relay Tracker web app'
+    now: moment(),
+    startTime: moment('2019-04-10T06:30:00-05:00')
+  },
+  created () {
+    setInterval(() => {
+      this.now = moment()
+    }, 1000)
+  },
+  computed: {
+    stopwatch: function () {
+      return this.now.diff(this.startTime)
+    },
+    formattedStopwatch: function () {
+      let duration = moment.duration(this.stopwatch)
+      return `${Math.floor(duration.asHours())} :
+        ${duration.minutes() < 10 ? 0 : ''}${duration.minutes()} :
+        ${duration.seconds() < 10 ? 0 : ''}${duration.seconds()}`
+    },
+    formattedStartTime: function () {
+      return this.startTime.format('MMMM Do YYYY, h:mm:ss A')
+    }
   }
 })
